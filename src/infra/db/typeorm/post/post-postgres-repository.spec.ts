4073e65bcbe0142bd936633e7af315c3ba60f5d5
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-11-22 22:49:49 -0300

    test: ensure PostPostgresRepository returns the post added on Add success

---
+++ src/infra/db/typeorm/post/post-postgres-repository.spec.ts
@@ -0,0 +1,41 @@
+import { AddPostParams } from "domain/usecase/add-todo";
+import { connection } from "../helper/typeorm-helper";
+import { PostPostgresRepository } from "./post-postgres-repository";
+
+const mockAddPostParams = (): AddPostParams => ({
+    title: "any_title",
+    description: "any_description",
+    body: "any_body",
+});
+
+type SutTypes = {
+    sut: PostPostgresRepository;
+};
+
+const makeSut = (): SutTypes => {
+    const sut = new PostPostgresRepository();
+    return {
+        sut,
+    };
+};
+
+describe("Post Postgres Repository", () => {
+    
+    beforeAll(async () => {
+        let migrations = await connection.create();
+        await migrations.runMigrations();
+    });
+
+    beforeEach(async () => {
+        await connection.clear();
+    });
+    afterAll(async () => {
+        await connection.close();
+    });
+    
+    test("Should be able to add a new Post", async () => {
+        const { sut } = makeSut();
+        const post = await sut.add(mockAddPostParams());
+        expect(post).toHaveProperty("id");
+    });
+});
