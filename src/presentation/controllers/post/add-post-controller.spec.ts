aae11ec6833f7b1003da25acba722dc3ccc67b76
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-11-23 06:35:37 -0300

    test: ensure AddPostController calls AddPost with the correct alues

---
+++ src/presentation/controllers/post/add-post-controller.spec.ts
@@ -0,0 +1,39 @@
+import { mockAddPost } from "@/domain/test/mock-post";
+import { AddPost, AddPostParams } from "@/domain/usecase/add-todo";
+import { HttpRequest } from "@/presentation/protocols/http";
+import { AddPostController } from "./add-post-controller";
+
+const makeFakeRequest = (): HttpRequest => ({
+    body: {
+        title: "any_title",
+        description: "any_description",
+        body: "any_body"
+    }
+})
+
+type SutTypes = {
+    sut: AddPostController,
+    addPostStub: AddPost
+}
+
+const makeSut = (): SutTypes => {
+    const addPostStub = mockAddPost();
+    const sut = new AddPostController(addPostStub);
+    return {
+        sut, 
+        addPostStub
+    }
+}
+
+describe("Add Post Controller", () => {
+    test("Should call AddPost with the correct values", async () => {
+        const { sut, addPostStub } = makeSut();
+        const addSpy = jest.spyOn(addPostStub, "add");
+        await sut.handle(makeFakeRequest());
+        expect(addSpy).toHaveBeenCalledWith({
+            title: "any_title",
+            description: "any_description",
+            body: "any_body"
+        })
+    });
+});
