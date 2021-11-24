7fa0b2f89b54c364f7b2ac64736c0a73c3d50c47
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-11-24 19:05:37 -0300

    feat: ensure PostPostgresRepository calls list() with the correct values

--- src/infra/db/typeorm/post/post-postgres-repository.ts
+++ src/infra/db/typeorm/post/post-postgres-repository.ts
@@ -1,10 +1,11 @@
 import { Post } from "@/domain/entities/post";
 import { PostModel } from "@/domain/model/post";
 import { AddPost, AddPostParams } from "@/domain/usecase/add-post";
+import { ListPostByDate, ListPostByDateParams } from "@/domain/usecase/list-post-by-date";
 import { UpdatePost, UpdatePostParams } from "@/domain/usecase/update-post";
-import { getRepository, Repository } from "typeorm";
+import { Between, getRepository, Repository } from "typeorm";
 
-export class PostPostgresRepository implements AddPost, UpdatePost {  
+export class PostPostgresRepository implements AddPost, UpdatePost, ListPostByDate{  
     
     private repository: Repository<Post>
     
@@ -26,6 +27,14 @@ export class PostPostgresRepository implements AddPost, UpdatePost {
         }
         const updatedPost = await this.repository.save({...post, ...data});
         return updatedPost;
+    }
+
+    async list(data: ListPostByDateParams): Promise<PostModel[]> {
+        const { start_date, end_date} = data;
+        const posts = await this.repository.find({
+            created_at: Between(start_date, end_date)
+        });
+        return null;
     }
 
 }   
