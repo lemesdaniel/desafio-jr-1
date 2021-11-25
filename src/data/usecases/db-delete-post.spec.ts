ba27c03986e8827624d306622cbcbc33b9da0245
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-11-24 22:06:50 -0300

    test: ensure DbDeletePost throw if DeletePostRepository throws

--- src/data/usecases/db-delete-post.spec.ts
+++ src/data/usecases/db-delete-post.spec.ts
@@ -23,4 +23,11 @@ describe("Db Delete Post", () => {
         await sut.delete("any_id");
         expect(deleteSpy).toHaveBeenCalledWith("any_id");
     });
+
+    test("Should throw if DeletePostRepository throws", async () => {
+        const { sut, deletePostRepositoryStub } = makeSut();
+        jest.spyOn(deletePostRepositoryStub, "delete").mockReturnValueOnce(Promise.reject(new Error()));
+        const promise = sut.delete("any_id");
+        await expect(promise).rejects.toThrow();
+    });
 });
