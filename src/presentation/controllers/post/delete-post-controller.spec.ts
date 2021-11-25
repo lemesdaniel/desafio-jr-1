519c3c6f19afc3ffcfc1c0b6725db864538e86d7
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-11-25 08:15:30 -0300

    test: ensure DeletePostController returns 500 if DeletePost throws

--- src/presentation/controllers/post/delete-post-controller.spec.ts
+++ src/presentation/controllers/post/delete-post-controller.spec.ts
@@ -36,4 +36,11 @@ describe("Delete Post Controller", () => {
         const response = await sut.handle(makeFakeRequest());
         expect(response.status).toBe(200);
     });
+
+    test("Should return 500 if DeletePost throws", async () => {
+        const { sut, deletePostStub } = makeSut();
+        jest.spyOn(deletePostStub, "delete").mockReturnValueOnce(Promise.reject(new Error()));
+        const response = await sut.handle(makeFakeRequest());
+        expect(response.status).toBe(500);
+    });
 });
