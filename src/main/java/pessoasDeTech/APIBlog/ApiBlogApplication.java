45d18891c425e2d60228fcaefe99dc993f18a3a2
Arthur Scarpati <arthur_159@hotmail.com>
2021-12-15 13:45:14 -0300

    Project created

---
+++ src/main/java/pessoasDeTech/APIBlog/ApiBlogApplication.java
@@ -0,0 +1,13 @@
+package pessoasDeTech.APIBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pessoasDeTech/APIBlog/ApiBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package pessoasDeTech.APIBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
